84ca55a6d1c67e255bd741456391a0b2b20f7f95
Rossen Hristov <rossen.hristov@telerik.com>
2016-06-10 17:28:32 +0300

    Fix the custom transition

--- app/custom-transition.ios.ts
+++ app/custom-transition.ios.ts
@@ -2,7 +2,13 @@
 
 export class CustomTransition extends transition.Transition {
     public animateIOSTransition(containerView: UIView, fromView: UIView, toView: UIView, operation: UINavigationControllerOperation, completion: (finished: boolean) => void): void {
-        toView.transform = CGAffineTransformMakeScale(0, 0);
+        let originalToViewTransform = toView.transform;
+        let originalFromViewTransform = fromView.transform;
+
+        //http://stackoverflow.com/questions/216076/uiview-scale-to-0-using-cgaffinetransformmakescale
+        let scaleTransform = CGAffineTransformMakeScale(0.0001, 0.0001);
+        
+        toView.transform = scaleTransform;
         fromView.transform = CGAffineTransformIdentity;
 
         switch (operation) {
@@ -14,12 +20,16 @@ export class CustomTransition extends transition.Transition {
                 break;
         }
 
-        var duration = this.getDuration();
-        var curve = this.getCurve();
+        let duration = this.getDuration();
+        let curve = this.getCurve();
         UIView.animateWithDurationAnimationsCompletion(duration, () => {
             UIView.setAnimationCurve(curve);
             toView.transform = CGAffineTransformIdentity;
-            fromView.transform = CGAffineTransformMakeScale(0, 0);
-        }, completion);
+            fromView.transform = scaleTransform;
+        }, (finished: boolean) => {
+            toView.transform = originalToViewTransform; 
+            fromView.transform = originalFromViewTransform; 
+            completion(finished);   
+        });
     }
 }
